51f03097abcf55ca769a8523ea461f2bcbfb88f3
Andrew <andrewHuang@richitech.com.tw>
2023-10-25 18:54:16 +0800

    publish切版內容確認

---
+++ assets/AboutView-7d522f72.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper-c27b6911.js";import{o,c as t}from"./index-e20c092a.js";const c={};function r(n,a){return o(),t("p",null,"This is an about page")}const f=e(c,[["render",r]]);export{f as default};
